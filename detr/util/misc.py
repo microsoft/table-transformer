66b02e39a6066b1d75f3feaf56c69a047a8314e5
Brandon Smock <brsmock@microsoft.com>
2022-04-01 17:19:31 -0700

    Fixed torchvision version checking

--- detr/util/misc.py
+++ detr/util/misc.py
@@ -11,6 +11,7 @@ from collections import defaultdict, deque
 import datetime
 import pickle
 from typing import Optional, List
+from packaging import version
 
 import torch
 import torch.distributed as dist
@@ -18,7 +19,7 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.7:
+if version.parse(torchvision.__version__) < version.parse('0.7'):
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
