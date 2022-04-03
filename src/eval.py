a9217a1378d80d5d0a32c9af680bdf1844fc752b
Brandon Smock <brsmock@microsoft.com>
2022-04-03 12:56:21 -0700

    Moving evaluate() from engine.py to eval.py

--- src/eval.py
+++ src/eval.py
@@ -20,7 +20,8 @@ import matplotlib.patches as patches
 from fitz import Rect
 
 sys.path.append("../detr")
-from engine import evaluate
+import util.misc as utils
+from datasets.coco_eval import CocoEvaluator
 import postprocess
 import grits
 from grits import grits_con, grits_top, grits_loc
@@ -523,7 +524,7 @@ def eval_tsr(args, model, dataset_test, device):
     st_time = datetime.now()
 
     torch.multiprocessing.set_start_method('spawn')
-    with multiprocessing.Pool(4) as pool:
+    with multiprocessing.Pool(2) as pool:
         all_metrics = list(tqdm.tqdm(pool.imap_unordered(partial(eval_tsr_sample,
                                                          table_words_dir=args.table_words_dir,
                                                          mode=args.mode, model=model,
@@ -543,6 +544,61 @@ def eval_tsr(args, model, dataset_test, device):
     print_metrics_summary(metrics_summary)
 
 
+@torch.no_grad()
+def evaluate(model, criterion, postprocessors, data_loader, base_ds, device):
+    model.eval()
+    criterion.eval()
+
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
+    header = 'Test:'
+
+    iou_types = tuple(k for k in ('segm', 'bbox') if k in postprocessors.keys())
+    coco_evaluator = CocoEvaluator(base_ds, iou_types)
+
+    for samples, targets in metric_logger.log_every(data_loader, 1000, header):
+        samples = samples.to(device)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+        outputs = model(samples)
+        loss_dict = criterion(outputs, targets)
+        weight_dict = criterion.weight_dict
+
+        # reduce losses over all GPUs for logging purposes
+        loss_dict_reduced = utils.reduce_dict(loss_dict)
+        loss_dict_reduced_scaled = {k: v * weight_dict[k]
+                                    for k, v in loss_dict_reduced.items() if k in weight_dict}
+        loss_dict_reduced_unscaled = {f'{k}_unscaled': v
+                                      for k, v in loss_dict_reduced.items()}
+        metric_logger.update(loss=sum(loss_dict_reduced_scaled.values()),
+                             **loss_dict_reduced_scaled,
+                             **loss_dict_reduced_unscaled)
+        metric_logger.update(class_error=loss_dict_reduced['class_error'])
+
+        orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
+        results = postprocessors['bbox'](outputs, orig_target_sizes)
+        res = {target['image_id'].item(): output for target, output in zip(targets, results)}
+        if coco_evaluator is not None:
+            coco_evaluator.update(res)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    if coco_evaluator is not None:
+        coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    if coco_evaluator is not None:
+        coco_evaluator.accumulate()
+        coco_evaluator.summarize()
+    stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+    if coco_evaluator is not None:
+        if 'bbox' in postprocessors.keys():
+            stats['coco_eval_bbox'] = coco_evaluator.coco_eval['bbox'].stats.tolist()
+
+    return stats, coco_evaluator
+
+
 def eval_coco(model, criterion, postprocessors, data_loader_test, dataset_test, device):
     """
     Use this function to do COCO evaluation. Default implementation runs it on
@@ -550,7 +606,7 @@ def eval_coco(model, criterion, postprocessors, data_loader_test, dataset_test, 
     """
     pubmed_stats, coco_evaluator = evaluate(model, criterion, postprocessors,
                                             data_loader_test, dataset_test,
-                                            device, None)
+                                            device)
     print("pubmed: AP50: {:.3f}, AP75: {:.3f}, AP: {:.3f}, AR: {:.3f}".format(
         pubmed_stats['coco_eval_bbox'][1], pubmed_stats['coco_eval_bbox'][2],
         pubmed_stats['coco_eval_bbox'][0], pubmed_stats['coco_eval_bbox'][8]))
