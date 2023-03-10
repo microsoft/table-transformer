182db93ca21e7abe82714669cf8068cbc1a0c5f8
Brandon Smock <smock.j.b@gmail.com>
2023-03-09 18:43:16 -0800

    Updating resize transform at inference

--- src/inference.py
+++ src/inference.py
@@ -37,13 +37,13 @@ class MaxResize(object):
         return resized_image
 
 detection_transform = transforms.Compose([
-    MaxResize(1000),
+    MaxResize(800),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
 structure_transform = transforms.Compose([
-    MaxResize(800),
+    MaxResize(1000),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
